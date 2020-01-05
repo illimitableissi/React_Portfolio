938c96d58b98a4dd9c065a8153d920dcb493dfbd
SOVEREIGN <isimemeni@gmail.com>
2020-01-05 10:50:05 -0500

    added app information

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -11,6 +11,7 @@ const Projects = () => {
                     <a href="https://illimitableissi.github.io/Psychic-Game/"><img src="images/placeholder1.jpg"></img></a>
                     <span className="card-title">Psychic Game</span>
                 </div>
+                    <div className="card-content"><p>The app randomly picks a letter, and the user has to guess which letter the app chose.</p></div>
                     <div className="card-action">
                       <a href="https://github.com/illimitableissi/Psychic-Game.git">See the Code</a>
                 </div>
@@ -22,6 +23,7 @@ const Projects = () => {
                     <a href="https://illimitableissi.github.io/unit-4-game/"><img src="images/placeholder2.jpg"></img></a>
                     <span className="card-title">Crystal Collector</span>
                 </div>
+                <div className="card-content"><p>The app randomly picks a number and the user will have to click on the stones and match the number.</p></div>
                     <div className="card-action">
                       <a href="https://github.com/illimitableissi/unit-4-game.git">See the Code</a>
                 </div>
@@ -33,6 +35,7 @@ const Projects = () => {
                     <a href="https://illimitableissi.github.io/TriviaGame/"><img src="images/placeholder3.jpg"></img></a>
                     <span className="card-title">Dragon Ball Trivia!</span>
                 </div>
+                <div className="card-content"><p>Dragon Ball Series trivia game.</p></div>
                 <div className="card-action">
                     <a href="https://github.com/illimitableissi/TriviaGame.git">See the Code</a>
                 </div>
@@ -46,6 +49,7 @@ const Projects = () => {
                     <a href="https://illimitableissi.github.io/GifTastic/"><img src="images/placeholder4.jpg"></img></a>
                     <span className="card-title">One Piece Pirates!</span>
                 </div>
+                <div className="card-content"><p>User can click on a character name and gifs will populate from the GIPHY API.</p></div>
                 <div className="card-action">
                     <a href="https://github.com/illimitableissi/GifTastic.git">See the Code</a>
                 </div>
@@ -57,6 +61,7 @@ const Projects = () => {
                     <a href="https://illimitableissi.github.io/TrainScheduler/"><img src="images/placeholder6.jpg"></img></a>
                     <span className="card-title">Train Scheduler</span>
                 </div>
+                <div className="card-content"><p>Incorporates Firebase to host arrival and departure data. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.</p></div>
                     <div className="card-action">
                       <a href="https://github.com/illimitableissi/TrainScheduler.git">See the Code</a>
                 </div>
@@ -68,6 +73,7 @@ const Projects = () => {
                     <a href="https://usafowose.github.io/Project-1/"><img src="images/placeholder7.jpg"></img></a>
                     <span className="card-title">TraveLite</span>
                 </div>
+                <div className="card-content"><p>Users can enter flight information and will be shown various flights, weather forecasts, and images of their chosen location.</p></div>
                     <div className="card-action">
                     <a href="https://github.com/usafowose/Project-1.git">See the Code</a>
                 </div>
@@ -81,6 +87,7 @@ const Projects = () => {
                     <a href="#"><img src="images/placeholder10.jpg"></img></a>
                     <span className="card-title">LIRI Bot-Node.js</span>
                 </div>
+                <div className="card-content"><p>Node app that allows users to see movie information, concerts, and music information from various APIs.</p></div>
                     <div className="card-action">
                     <a href="https://github.com/illimitableissi/liri-node-app.git">See the Code</a>
                 </div>
@@ -92,6 +99,7 @@ const Projects = () => {
                     <a href="#"><img src="images/placeholder11.jpg"></img></a>
                     <span className="card-title">MySQL+Node.js Bamazon</span>
                 </div>
+                <div className="card-content"><p>Back-end storefront built with MySQL.</p></div>
                     <div className="card-action">
                     <a href="https://github.com/illimitableissi/bamazon.git">See the Code</a>
                 </div>
@@ -103,6 +111,7 @@ const Projects = () => {
                     <a href="https://friend-finderi.herokuapp.com/"><img src="images/placeholder12.jpg"></img></a>
                     <span className="card-title">Friend Finder</span>
                 </div>
+                <div className="card-content"><p>This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.</p></div>
                     <div className="card-action">
                     <a href="https://github.com/illimitableissi/FriendFinder.git">See the Code</a>
                 </div>
@@ -116,6 +125,7 @@ const Projects = () => {
                     <a href="https://burgerii.herokuapp.com/"><img src="images/placeholder13.jpg"></img></a>
                     <span className="card-title">Eat Da Burger!</span>
                 </div>
+                <div className="card-content"><p>Restaurant app that lets users input the names of burgers they'd like to eat.</p></div>
                     <div className="card-action">
                     <a href="https://github.com/illimitableissi/burger">See the Code</a>
                 </div>
@@ -127,6 +137,7 @@ const Projects = () => {
                     <a href="https://thekrustykrab.herokuapp.com/"><img src="images/placeholder14.jpg"></img></a>
                     <span className="card-title">The Krusty Krab</span>
                 </div>
+                <div className="card-content"><p>Spongebob Squarepants themed restaurant app.</p></div>
                     <div className="card-action">
                     <a href="https://github.com/illimitableissi/Project-2">See the Code</a>
                 </div>
