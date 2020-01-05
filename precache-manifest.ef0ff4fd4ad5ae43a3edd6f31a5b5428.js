f694555e247362ac8134c758b928d2df164774c2
SOVEREIGN <isimemeni@gmail.com>
2020-01-04 23:42:13 -0500

    Updates

---
+++ precache-manifest.ef0ff4fd4ad5ae43a3edd6f31a5b5428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4237b71aa59a74cef568318a7508e6",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "4459d89e30bd423fe2e0",
+    "url": "/React_Portfolio/static/css/main.5a3408ae.chunk.css"
+  },
+  {
+    "revision": "db3634cce2569c4fe263",
+    "url": "/React_Portfolio/static/js/2.c2a6f293.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_Portfolio/static/js/2.c2a6f293.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4459d89e30bd423fe2e0",
+    "url": "/React_Portfolio/static/js/main.155b158d.chunk.js"
+  },
+  {
+    "revision": "10489b775eee9b5ec2b2",
+    "url": "/React_Portfolio/static/js/runtime-main.8de0d406.js"
+  }
+]);
